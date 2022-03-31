44e753957d83f1b1deac439528314e1bfcdd6d40
gorazd007 <gorazdtezak@gmail.com>
2022-03-31 20:31:41 +0200

    exercise in Thursday

---
+++ repo04/ex05/src/index.js
@@ -0,0 +1,17 @@
+function main(numOne, numTwo){
+
+    var numOne = 2.5;
+    var numTwo = 2;
+
+
+
+    product = numOne * numTwo
+
+
+
+    return product;
+
+}
+
+console.log(main(numOne, numTwo));
+module.exports = main
